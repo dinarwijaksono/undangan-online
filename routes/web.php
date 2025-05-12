a9fe5d4d9fd00867ecfe3cbb7840525a6306ded6
dinar wijaksono <dinarwijaksono11@gmail.com>
2025-05-13 00:22:01 +0700

    Desain: setup halaman auth.login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+// AuthController
+Route::get('/login', [AuthController::class, 'login']);
