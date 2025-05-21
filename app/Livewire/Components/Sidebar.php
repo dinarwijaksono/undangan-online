0d51ca1878155fc496397f9ac809a73ab6ffedcd
dinar wijaksono <dinarwijaksono11@gmail.com>
2025-05-21 23:14:11 +0700

    Feature: menambahkan logic menu aktiv

--- app/Livewire/Components/Sidebar.php
+++ app/Livewire/Components/Sidebar.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class Sidebar extends Component
 {
+    public $active = 'dashboard';
+
+    public function boot()
+    {
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+
+        $this->active = $path[1] ?? 'main';
+    }
+
     public function render()
     {
         return view('livewire.components.sidebar');
--- resources/views/livewire/components/sidebar.blade.php
+++ resources/views/livewire/components/sidebar.blade.php
@@ -1,8 +1,11 @@
 <sidebar class="basis-2/12 py-4 px-2">
     <ul class="menu rounded-box w-full">
-        <li><a href="/main" class="btn btn-primary mb-2">Beranda</a></li>
-        <li><a href="/invitation/my-invitation" class="btn btn-secondary-content mb-2">Undanganku</a></li>
-        <li><a href="/invitation-template" class="btn btn-secondary-content mb-2">Templetku</a></li>
-        <li><a href="/confirm-attendance" class="btn btn-secondary-content mb-2">Konfirmasi kehadiran</a></li>
+        <li><a href="/main" @class(['btn mb-2', 'btn-primary' => $active == 'main'])>Beranda</a></li>
+        <li><a href="/invitation/my-invitation" @class(['btn mb-2', 'btn-primary' => $active == 'invitation'])>Undanganku</a></li>
+        <li><a href="/invitation-template" @class([
+            'btn mb-2',
+            'btn-primary' => $active == 'invitation-template',
+        ])>Templetku</a></li>
+        <li><a href="/confirm-attendance" @class(['btn mb-2', 'btn-primary' => $active == 'confirm-attendance'])>Konfirmasi kehadiran</a></li>
     </ul>
 </sidebar>
